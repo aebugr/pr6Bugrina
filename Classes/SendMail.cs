5fccea8a54029b762b81f74d7917c7a1451ee5d4
HP <HP@DESKTOP-T6A34MC>
2024-12-11 23:56:29 +0500

    Разработанный класс отправки сообщений на почту. Бугрина

---
+++ Classes/SendMail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegINBugrina.Classes
+{
+    public class SendMail
+    {
+        public static void SendMessage(string Message, string To)
+        {
+            var smtpClient = new SmtpClient("smtp.yandex.ru")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("TestKylosov@yandex.ru", "ekdzbcygzmqoomcx"),
+                EnableSsl = true,
+            };
+            smtpClient.Send("Bugrina@yandex.ru", To, "Проект RegIn", Message);
+        }
+    }
+}
